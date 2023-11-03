99c4dd470622e6ace57e7075a92e3cdea25753ab
FabockDays <fabiandz.91@hotmail.com>
2023-11-03 09:36:29 -0300

    proyecto finalizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
         <div>
             <Link to='/'>
                 <img
-                    src='https://scontent.faep7-1.fna.fbcdn.net/v/t39.30808-1/302131636_387880296839676_6384932597580772018_n.jpg?stp=dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=CwffH9CI8hgAX9m39Hr&_nc_ht=scontent.faep7-1.fna&oh=00_AfCMbhzYItV6vFFGyxp1fkkk8g-MPRxsUgJJH39-PV4ZxQ&oe=65334BA0'
+                    src='https://scontent.faep7-1.fna.fbcdn.net/v/t39.30808-1/302131636_387880296839676_6384932597580772018_n.jpg?stp=dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHib4jGo7reK9rX-n7xbsS05gznqfR0vZ_mDOep9HS9ny0doZ4HylbtRDKzFx4-tKU&_nc_ohc=nhOzDdFbs-kAX-hAdQA&_nc_ht=scontent.faep7-1.fna&oh=00_AfCWNlO49l4vLUyVqe3-tuHLTLkaSF3wcDYAzmXqJxtdYQ&oe=65490C60'
                     alt='Logo'
                 />
             </Link>
